c276429d4d925c397bee673230a4ce22267c86c5
hatelove <jay.hatelove@gmail.com>
2017-09-28 15:17:02 +0800

    add first failed test case: Love All

---
+++ TennisKata/TennisGameTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisKata
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            var tennisGame = new TennisGame();
+            string result = tennisGame.ShowScore();
+            Assert.AreEqual("Love All", result);
+        }
+    }
+
+    public class TennisGame
+    {
+        public string ShowScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
