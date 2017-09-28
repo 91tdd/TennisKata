34f6c215d52b62754186647a0e5a5a06b73f8da6
hatelove <jay.hatelove@gmail.com>
2017-09-28 15:54:00 +0800

    refactor: extract method GetPlayer1Score()

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -6,20 +6,32 @@
 
         public string ShowScore()
         {
-            if (this._player1ScoreTimes == 1)
+            if (this._player1ScoreTimes > 0)
             {
-                return "Fifteen Love";
-            }
-            else if (this._player1ScoreTimes == 2)
-            {
-                return "Thirty Love" ;
-            }
-            else if (this._player1ScoreTimes == 3)
-            {
-                return "Forty Love";
+                var player1Score = GetPlayer1Score();
+
+                return player1Score + " Love";
             }
 
             return "Love All";
+        }
+
+        private string GetPlayer1Score()
+        {
+            var player1Score = "";
+            if (this._player1ScoreTimes == 1)
+            {
+                player1Score = "Fifteen";
+            }
+            else if (this._player1ScoreTimes == 2)
+            {
+                player1Score = "Thirty";
+            }
+            else if (this._player1ScoreTimes == 3)
+            {
+                player1Score = "Forty";
+            }
+            return player1Score;
         }
 
         public void Player1Score()
